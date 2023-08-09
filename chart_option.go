a3053d02ec99fbc32bc7bbe35bbed59a957b5337
Ben <ben@swit.io>
2023-08-09 17:20:40 +0900

    feat(realtime): add charts

--- chart_option.go
+++ chart_option.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-type TickChartOption struct {
+type TickSectorChartOption struct {
 	Code SectorCode
 	// 요청건수 (최대-압축: 2000 비압축:500)
 	QueryCount int
@@ -18,11 +18,11 @@ type TickChartOption struct {
 	Compress  bool
 }
 
-func (TickChartOption) String() string {
+func (TickSectorChartOption) String() string {
 	return "t8417"
 }
 
-func (t TickChartOption) MarshalJSON() ([]byte, error) {
+func (t TickSectorChartOption) MarshalJSON() ([]byte, error) {
 	return []byte(fmt.Sprintf(`{
   "%sInBlock": {
     "shcode": "%s",
@@ -44,6 +44,210 @@ func (t TickChartOption) MarshalJSON() ([]byte, error) {
 	)), nil
 }
 
-func (t TickChartOption) Path() string {
+func (t TickSectorChartOption) Path() string {
 	return "/indtp/chart"
+}
+
+// 기간별투자자매매추이(챠트)
+type InvestorTrendChartOption struct {
+	// 1:코스피 2:KP200 3:코스닥 4:선물 5:풋옵션 6:콜옵션
+	Market string
+	// 업종코드
+	SectorCode
+	// 수치구분
+	// 1: 수치, 2: 누적
+	AmountType string
+	// 1:일,2:주,3:월
+	UnitType  string
+	StartDate string
+	EndDate   string
+}
+
+func (InvestorTrendChartOption) String() string {
+	return "t1665"
+}
+
+func (InvestorTrendChartOption) Path() string {
+	return "/stock/chart"
+}
+
+func (t InvestorTrendChartOption) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`{
+  "%sInBlock": {
+	"market": "%s",
+	"upcode": "%s",
+	"gubun2": "%s",
+	"gubun3": "%s",
+	"from_date": "%s",
+	"to_date": "%s"
+  }
+}`, t.String(),
+		t.Market,
+		t.SectorCode,
+		t.AmountType,
+		t.UnitType,
+		t.StartDate,
+		t.EndDate,
+	)), nil
+}
+
+// API전용주식챠트(일주월년)
+type StockChartOption struct {
+	Ticker string
+	// 2:일 3:주 4:월 5:년
+	Period string
+	// 최대압축 2000 비압축 500
+	Count     int
+	StartDate string
+	EndDate   string
+	// 압축여부
+	Compress bool
+	// 수정주가 여부
+	Edited bool
+}
+
+func (StockChartOption) String() string {
+	return "t8410"
+}
+
+func (StockChartOption) Path() string {
+	return "/stock/chart"
+}
+
+func (t StockChartOption) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`{
+  "%sInBlock": {
+	"shcode": "%s",
+	"gubun": "%s",
+	"qrycnt": %d,
+	"sdate": "%s",
+	"edate": "%s",
+	"comp_yn": "%s",
+	"sudung": "%s"
+  }
+}`, t.String(),
+		t.Ticker,
+		t.Period,
+		t.Count,
+		t.StartDate,
+		t.EndDate,
+		func() string {
+			if t.Compress {
+				return "Y"
+			}
+			return "N"
+		}(),
+		func() string {
+			if t.Edited {
+				return "Y"
+			}
+			return "N"
+		}(),
+	)), nil
+}
+
+type TickChartOption struct {
+	Ticker string
+	NTick  int
+	Count  int
+	// 조회영업일수(0:미사용1>=사용)
+	NDay          int
+	StartDate     string
+	EndDate       string
+	ContinuesDate string
+	ContinuesTime string
+	Compress      bool
+}
+
+func (TickChartOption) String() string {
+	return "t8411"
+}
+
+func (TickChartOption) Path() string {
+	return "/stock/chart"
+}
+
+func (t TickChartOption) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`{
+  "%sInBlock": {
+	"shcode": "%s",
+	"ncnt": %d,
+	"qrycnt": %d,
+	"nday": "%d",
+	"sdate": "%s",
+	"edate": "%s",
+	"cts_date": "%s",
+	"cts_time": "%s",
+	"comp_yn": "%s"
+  }
+}`, t.String(),
+		t.Ticker,
+		t.NTick,
+		t.Count,
+		t.NDay,
+		t.StartDate,
+		t.EndDate,
+		t.ContinuesDate,
+		t.ContinuesTime,
+		func() string {
+			if t.Compress {
+				return "Y"
+			}
+			return "N"
+		}(),
+	)), nil
+}
+
+type MinChartOption struct {
+	Ticker string
+	// n분
+	MinType int
+	// 최대압축 2000 비압축 500
+	Count int
+	// 	조회영업일수(0:미사용1>=사용)
+	NDay          string
+	StartDate     string
+	EndDate       string
+	ContinuesDate string
+	ContinuesTime string
+	Compress      bool
+}
+
+func (MinChartOption) String() string {
+	return "t8412"
+}
+
+func (MinChartOption) Path() string {
+	return "/stock/chart"
+}
+
+func (t MinChartOption) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`{
+  "%sInBlock": {
+	"shcode": "%s",
+	"ncnt": %d,
+	"qrycnt": %d,
+	"nday": "%s",
+	"sdate": "%s",
+	"edate": "%s",
+	"cts_date": "%s",
+	"cts_time": "%s",
+	"comp_yn": "%s"
+  }
+}`, t.String(),
+		t.Ticker,
+		t.MinType,
+		t.Count,
+		t.NDay,
+		t.StartDate,
+		t.EndDate,
+		t.ContinuesDate,
+		t.ContinuesTime,
+		func() string {
+			if t.Compress {
+				return "Y"
+			}
+			return "N"
+		}(),
+	)), nil
 }
--- charts_test.go
+++ charts_test.go
@@ -24,4 +24,59 @@ func Test_Charts(t *testing.T) {
 		require.NoError(t, err)
 	})
 
+	t.Run("InvestorTrendChartOption", func(t *testing.T) {
+		_, err := cli.Execute(context.TODO(), InvestorTrendChartOption{
+			Market:     "1",
+			SectorCode: "001",
+			AmountType: "1",
+			UnitType:   "1",
+			StartDate:  "20230604",
+			EndDate:    time.Now().Format("20060102"),
+		})
+		require.NoError(t, err)
+	})
+
+	t.Run("StockChartOption", func(t *testing.T) {
+		_, err := cli.Execute(context.TODO(), StockChartOption{
+			Ticker:    "005930",
+			Period:    "2",
+			Count:     0,
+			StartDate: "20230701",
+			EndDate:   time.Now().Format("20060102"),
+			Compress:  false,
+			Edited:    true,
+		})
+		require.NoError(t, err)
+	})
+
+	t.Run("TickChartOption", func(t *testing.T) {
+		_, err := cli.Execute(context.TODO(), TickChartOption{
+			Ticker:        "005930",
+			NTick:         1,
+			Count:         0,
+			NDay:          0,
+			StartDate:     " ",
+			EndDate:       " ",
+			ContinuesDate: "",
+			ContinuesTime: "",
+			Compress:      false,
+		})
+		require.NoError(t, err)
+	})
+
+	t.Run("MinChartOption", func(t *testing.T) {
+		_, err := cli.Execute(context.TODO(), MinChartOption{
+			Ticker:        "005930",
+			MinType:       3,
+			Count:         0,
+			NDay:          "0",
+			StartDate:     "20230701",
+			EndDate:       time.Now().Format("20060102"),
+			ContinuesDate: "",
+			ContinuesTime: "",
+			Compress:      false,
+		})
+		require.NoError(t, err)
+	})
+
 }
